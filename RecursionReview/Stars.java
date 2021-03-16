fa4d0886e7710d3aa237b8b865e36340c6c36794
Sharon Hsieh <55104709+SharonHsieh22@users.noreply.github.com>
2021-03-15 22:16:30 -0700

    Add files via upload

--- RecursionReview/Stars.java
+++ RecursionReview/Stars.java
@@ -1,10 +1,28 @@
 public class Stars {
     public static void main(String[] args) {
-        System.out.println(triangle(5));
+       System.out.println(triangle(5));
+       System.out.println(reverseTriangle(5));
+    }
+
+    public static void printStars(int stars) {
+        for (int i = 0; i < stars; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }       
+    
+    public static String triangle(int n) {
+        if(n < 1) return "";
+        printStars(n);
+        return triangle(n-1);
     }
     
-    public static String triangle(int stars) {
-        if(stars < 1) return "";
-        return "*" + triangle(stars-1);        
+    public static String reverseTriangle(int n) { 
+        if(n < 1) return "";
+        String l = reverseTriangle(n-1); 
+        printStars(n);
+        return l;
+        
     }
 }
+
